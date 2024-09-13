bf5e0ffa5dfc6e3b5c68f2d239b0827004576012
Philippe Serhal <philippe.serhal@netlify.com>
2024-09-13 10:44:55 -0400

    feat: initial commit of nuxt starter

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-09-13",
+  future: { compatibilityVersion: 4 },
+  devtools: { enabled: true },
+});
