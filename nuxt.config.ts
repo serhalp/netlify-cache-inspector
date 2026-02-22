e6d4e2cd8404cd3afb75b117da5b0c187f61eba7
Philippe Serhal <philippe.serhal@gmail.com>
2026-02-22 13:57:45 -0500

    feat: add a proper favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,34 @@
 import { resolve } from 'node:path'
 
 export default defineNuxtConfig({
-
-  modules: ['@unocss/nuxt', '@nuxt/eslint', '@nuxt/test-utils/module', '@netlify/nuxt'],
+  modules: [
+    '@unocss/nuxt',
+    '@nuxt/eslint',
+    '@nuxt/test-utils/module',
+    '@netlify/nuxt',
+  ],
   devtools: { enabled: true },
 
   app: {
     head: {
       title: 'Netlify Cache Inspector',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-96x96.png',
+          sizes: '96x96',
+        },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+      meta: [{ name: 'apple-mobile-web-app-title', content: 'ntlcache' }],
       script: [
         {
           // Prevent FOUC: apply dark class before first paint
